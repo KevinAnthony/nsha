719d47cbd915f2f4cb25086dd24308734e111389
Kevin Anthony <kevin.s.anthony@gmail.com>
2025-03-07 16:47:01 -0500

    shift

--- coin_counter/coin_counter.ino
+++ coin_counter/coin_counter.ino
@@ -1,13 +1,20 @@
 #include <Wire.h>
 #include "lanesensor.h"
 #include "MatrixOrbitali2c.h"
+#include "ShiftIn.h"
 
-LaneSensor _q(10,25);
-LaneSensor _d(7,10);
-LaneSensor _n(9,5);
-LaneSensor _p(8,1);
+int buttonLoad = 19;
+int buttonCE = 20;
+int buttonData = 21;
+int buttonClock = 22;
+
+LaneSensor _q(10, 25);
+LaneSensor _d(7, 10);
+LaneSensor _n(9, 5);
+LaneSensor _p(8, 1);
 unsigned long elapsed = 0;
-unsigned long RunDelay  = 3000;
+unsigned long RunDelay = 3000;
+ShiftIn<1> shift;
 MatrixOrbitali2c lcd(0x28);
 
 void check(LaneSensor* c) {
@@ -17,37 +24,46 @@ void check(LaneSensor* c) {
   }
 }
 
-void refreshDisplay(){
+void refreshDisplay() {
   size_t err;
   err = lcd.write(_q.String());
   if (err > 0) {
     Serial.println("error");
     Wire.end();
     delay(10);
-    lcd.begin(4,20);
+    lcd.begin(4, 20);
     return;
   }
-  
+
   lcd.write(_d.String());
   lcd.write(_n.String());
   lcd.write(_p.String());
 }
 
 void setup() {
-  lcd.begin(4,20);
+  lcd.begin(4, 20);
   Serial.begin(9600);
+
+  shift.begin(buttonLoad, buttonCD, buttonData, buttonClock);
 }
 
 void loop() {
+
+  if (shift.update()) {  // read in all values. returns true if any button has changed
+    for (int i = 0; i < shift.getDataWidth(); i++)
+      Serial.print(shift.state(i));  // get state of button i
+    Serial.println();
+  }
+
+  delay(1);
   // check(_q);
   // check(_n);
   // check(_p);
   // check(_d);
 
-  refreshDisplay();
+  // refreshDisplay();
 
   // if (elapsed > 0 && millis() - elapsed > RunDelay) {
   //   elapsed = 0;
   // }
 }
-
