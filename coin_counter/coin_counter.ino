cedca0072d0055bcb750f7190e54ca0ff4af6d99
Kevin <kevin.s.anthony@gmail.com>
2025-03-06 23:12:18 -0500

    coin cointer v2

--- coin_counter/coin_counter.ino
+++ coin_counter/coin_counter.ino
@@ -1,11 +1,14 @@
+#include <Wire.h>
 #include "lanesensor.h"
+#include "MatrixOrbitali2c.h"
 
-LaneSensor* _q;
-LaneSensor* _d;
-LaneSensor* _n;
-LaneSensor* _p;
+LaneSensor _q(10,25);
+LaneSensor _d(7,10);
+LaneSensor _n(9,5);
+LaneSensor _p(8,1);
 unsigned long elapsed = 0;
 unsigned long RunDelay  = 3000;
+MatrixOrbitali2c lcd(0x28);
 
 void check(LaneSensor* c) {
   if (c->CheckCount()) {
@@ -14,22 +17,37 @@ void check(LaneSensor* c) {
   }
 }
 
+void refreshDisplay(){
+  size_t err;
+  err = lcd.write(_q.String());
+  if (err > 0) {
+    Serial.println("error");
+    Wire.end();
+    delay(10);
+    lcd.begin(4,20);
+    return;
+  }
+  
+  lcd.write(_d.String());
+  lcd.write(_n.String());
+  lcd.write(_p.String());
+}
+
 void setup() {
+  lcd.begin(4,20);
   Serial.begin(9600);
-  _q = new LaneSensor(10, 25);
-  _d = new LaneSensor(7, 10);
-  _n = new LaneSensor(9, 5);
-  _p = new LaneSensor(8, 1);
 }
 
 void loop() {
-  check(_q);
-  check(_n);
-  check(_p);
-  check(_d);
+  // check(_q);
+  // check(_n);
+  // check(_p);
+  // check(_d);
 
-  if (elapsed > 0 && millis() - elapsed > RunDelay) {
-    elapsed = 0;
-  }
+  refreshDisplay();
+
+  // if (elapsed > 0 && millis() - elapsed > RunDelay) {
+  //   elapsed = 0;
+  // }
 }
 
---
+++ coin_counter/electronics/fab.bat
@@ -0,0 +1,3 @@
+kikit separate --source "annotation; ref: Main" coin_counter.kicad_pcb fab/main.kicad_pcb
+kikit separate --source "annotation; ref: Button" coin_counter.kicad_pcb fab/button.kicad_pcb
+kikit separate --source "annotation; ref: Sensor" coin_counter.kicad_pcb fab/sensor.kicad_pcb
