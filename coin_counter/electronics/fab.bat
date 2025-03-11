4bd4ddedf5f0a7618894aa281f483ec061e9ec2a
Kevin Anthony <kevin.s.anthony@gmail.com>
2025-03-11 07:54:13 -0400

    add rule checker

--- coin_counter/electronics/fab.bat
+++ coin_counter/electronics/fab.bat
@@ -1,3 +1,5 @@
+kikit drc run --level error coin_counter.kicad_pcb
+
 kikit separate --source "annotation; ref: Main" coin_counter.kicad_pcb fab/main.kicad_pcb
 kikit separate --source "annotation; ref: Button" coin_counter.kicad_pcb fab/button.kicad_pcb
 kikit separate --source "annotation; ref: Sensor" coin_counter.kicad_pcb fab/sensor.kicad_pcb
